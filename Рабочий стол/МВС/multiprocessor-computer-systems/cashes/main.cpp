66432fd381371ef44537e35b11f00030502f6030
nestyme <zueva.nn@phystech.edu>
2017-12-16 04:36:09 +0300

    upload first task

--- Рабочий стол/МВС/multiprocessor-computer-systems/cashes/main.cpp
+++ Рабочий стол/МВС/multiprocessor-computer-systems/cashes/main.cpp
@@ -1,57 +1,47 @@
-#include <iostream>
-#include <vector>
-#include <fstream>
-#include <chrono>
-#define  blockSize 4096
-#include <algorithm>
+#include <stdio.h>
+#include <ctime>
+#define N 400000000
+
 using namespace std;
-using namespace chrono;
-ofstream strm("/home/nestyme/Рабочий стол/МВС/output.txt");
-double blockReading(unsigned int *data, unsigned int n,
-                    unsigned int cnt,
-                    unsigned int block_size = blockSize) {
-	steady_clock::time_point begin = steady_clock::now();
-    	for (auto j = 0; j < n; j += block_size) {
-            unsigned int s = 0;
-		    for (auto k = j; k < j + cnt; ++k) {
-			    s += data[k];
-		    }
-	}
-	steady_clock::time_point end = steady_clock::now();
-	auto PassedTime = duration_cast<microseconds>(end - begin).count();
-	strm << "разница во времени (в тактах работы) = " << PassedTime <<endl;
-	return PassedTime;
-}
 
-
-void readDataBlocks()
-{
-    unsigned int n;
-    n = 1 << 27;
-    auto * data = (unsigned int *)calloc(n, sizeof(int));
-    int i;
-    for (i = 0; i < n; ++i) {
-        data[i] = rand() % 100;
-    }
-
-    for (auto cnt = 1; cnt <= 65; ++cnt) {
-        vector<double> seconds;
-        unsigned int i1;
-        i1 = 100;
-        double s;
-        s = 0;
-        for (auto i = 0; i < i1; ++i) {
-            auto t = blockReading(data, n, cnt);
-            seconds.push_back(t);
-            s += t;
+int memoryBank[N];
+bool flag = 1;
+void CasheInvestigation()
+    {
+    for (unsigned  int w = 1; w < 30; ++w)
+    {
+        for (unsigned int CasheSize = 24; CasheSize <= 24; CasheSize += 4)
+        {
+            unsigned int place = 0;
+            for (unsigned int j = 0; j < w; ++j)
+            {
+                for (unsigned int i = 0; i < (1 << CasheSize); ++i)
+                {
+                    int r1 = j * (1 << CasheSize) + i;
+                    int r2= (j + 1) * (1 << CasheSize) + i;
+                    memoryBank[r1] = r2;
+                }
+            }
+            for (unsigned int i = 0; i < (1 << CasheSize); ++i)
+            {
+                memoryBank[w * (1 << CasheSize) + i] = i;
+            }
+            unsigned long long t = clock();
+            place = 0;
+            for (unsigned int i = 0; i < (1 << 25); ++i)
+            {
+                place = memoryBank[place];
+            }
+            if (flag == 1)
+            {
+                        flag -= 1;
+                    printf("%d\n", CasheSize);
+                }
+            printf("%d %lld \n", w, clock()-t);
         }
-        s /= i1;
-
-        strm << cnt << " " << s << endl;
     }
 }
-
 int main()
 {
-    readDataBlocks();
+   CasheInvestigation();
 }
